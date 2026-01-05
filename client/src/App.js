e755ee059aa530fd2d255136249a73a3a0c7cee7
Chandra Shekhar  Verma <vermachandrashekhar17@gmail.com>
2026-01-06 02:38:30 +0530

    FrontEnd Initialization

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center text-xl font-semibold mt-10 text-blue-600">
+      Chat App Frontend Initialized
+    </div>
+  );
+}
+
+export default App;
