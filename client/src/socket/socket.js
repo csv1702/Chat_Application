03023f85191388c3d77da32b6b96a7c034a50dc0
Chandra Shekhar  Verma <vermachandrashekhar17@gmail.com>
2026-01-21 03:28:40 +0530

    Deployment Ready

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -2,10 +2,14 @@ import { io } from "socket.io-client";
 
 let socket = null;
 
+const SOCKET_URL = process.env.REACT_APP_API_URL 
+  ? process.env.REACT_APP_API_URL.replace("/api", "")
+  : "http://localhost:5000";
+
 export const connectSocket = () => {
   if (socket) return socket;
 
-  socket = io("http://localhost:5000", {
+  socket = io(SOCKET_URL, {
     withCredentials: true,
     transports: ["websocket"],
   });
